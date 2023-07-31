ad0b35737911c7333a6171a21e724f7d183cbdd0
adhiduzz <104109164+adhiduzz@users.noreply.github.com>
2023-07-31 21:26:43 +0700

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -17,7 +17,7 @@ module.exports = {
   kvPrefix: process.env.KV_PREFIX || '',
 
   // The name of your website. Present alongside your icon.
-  title: "Adhiduzz's OneDrive",
+  title: "Adhiduzz's Storage",
 
   // The folder that you are to share publicly with onedrive-vercel-index. Use '/' if you want to share your root folder.
   baseDirectory: '/plm',
